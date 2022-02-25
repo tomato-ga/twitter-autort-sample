34cb0a5e56d4f0c181235fbfbf92855b62a1286f
tomato <servicedake@gmail.com>
2022-02-25 13:24:18 +0900

    無駄なコードを削除

--- twitter-autort.py
+++ twitter-autort.py
@@ -11,7 +11,6 @@ Bearer_token = '自分のAPI'
 client = tweepy.Client(consumer_key=API_KEY, consumer_secret=API_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET, bearer_token=Bearer_token)
 
 # 検索結果をリツイートしたい場合
-
 # 検索したいキーワード入力
 q = 'キーワード入力'
 
@@ -21,7 +20,7 @@ counts = 10
 # search_result = client.search_recent_tweets(query=q, max_results=counts)
 
 # リスト取得 非公開リストは取得NG 公開設定にする
-search_result = client.get_list_tweets(id="リストIDをここに入力", max_results=counts) # expansions=attachments.media_keys
+search_result = client.get_list_tweets(id="リストIDをここに入力", max_results=counts)
 tweetids = search_result.data
 
 for tweetid in tweetids:
