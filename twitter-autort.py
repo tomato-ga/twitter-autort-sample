b360e0b0c6133faa267eedf2147b440827dd174f
tomato <52771764+tomato-ga@users.noreply.github.com>
2022-02-25 12:03:18 +0900

    Update twitter-autort.py

--- twitter-autort.py
+++ twitter-autort.py
@@ -10,11 +10,15 @@ Bearer_token = '自分のAPI'
 # Twitter API v2対応
 client = tweepy.Client(consumer_key=API_KEY, consumer_secret=API_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET, bearer_token=Bearer_token)
 
+# 検索結果をリツイートしたい場合
+
 # 検索したいキーワード入力
 q = 'キーワード入力'
 
 # 取得したい件数
 counts = 10
+
+# search_result = client.search_recent_tweets(query=q, max_results=counts)
 
 # リスト取得 非公開リストは取得NG 公開設定にする
 search_result = client.get_list_tweets(id="リストIDをここに入力", max_results=counts) # expansions=attachments.media_keys
